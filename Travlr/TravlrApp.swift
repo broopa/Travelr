9956c783b8fa4a9b5d898bd0db958578fe151000
Ian-CM <30392014+broopa@users.noreply.github.com>
2022-06-07 16:21:20 -0400

    Initial Push
    Hello World Indeed

---
+++ Travlr/TravlrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravlrApp.swift
+//  Travlr
+//
+//  Created by Ian Albahae on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TravlrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
